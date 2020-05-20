496b036f408d9e9721d494463f0474a8df4e3036
BuildTools <unconfigured@null.spigotmc.org>
2020-05-20 15:09:43 +0200

    Asteroid: Continued working on bullets

--- asteroids_test/version3 - Collision response/game/bullet.py
+++ asteroids_test/version3 - Collision response/game/bullet.py
@@ -7,8 +7,12 @@ class Bullet(physicalobject.PhysicalObject):
 
     def __init__(self, *args, **kwargs):
         super(Bullet, self).__init__(resources.bullet_image, *args, **kwargs)
-        pyglet.clock.schedule_once(self.die, 0.5)
 
+        # Bullets shouldn't stick around forever
+        # pyglet.clock.schedule_once(self.die, 0.5)
+
+        # Flag as a bullet
+        self.is_bullet = True
 
     def die(self, dt):
         self.dead = True
