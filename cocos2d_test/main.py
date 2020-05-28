763958d7a44a8096c47be77d518ccac694f2f6ad
Agardh <benjamin.agardh@gmail.com>
2020-05-28 20:23:11 +0200

    Decided to give up cocos2d

--- cocos2d_test/main.py
+++ cocos2d_test/main.py
@@ -1,21 +1,13 @@
+import pyglet
 import cocos
+from cocos.director import director
 
-class HelloWorld(cocos.layer.Layer):
-    def __init__(self):
-        super(HelloWorld, self).__init__()
-
-        label = cocos.text.Label("Hello, world!", 
-                                 font_name="Times New Roman", 
-                                 font_size=32, anchor_x="center", anchor_y="center")
-        self.add(label)
+class TileMap(cocos.tiles.RectMapLayer):
+    def __init__(self, ID, tw, th, cells):
+        super(TileMap, self).__init__(ID, tw, th, cells)
 
 
 
-cocos.director.director.init()
-
-
-hello = HelloWorld()
-
-main_scene = cocos.scene.Scene(hello)
-
-cocos.director.director.run(main_scene)
+director.init(resizable=True)
+# Run a scene with our event displayers:
+director.run( cocos.scene.Scene(TileMap(0, 100,100,  [["a", "d"], ["b", "e"], ["c", "f"]])))
