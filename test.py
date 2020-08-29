b11e624af3f2a377b9303bdd60bf9cb578c2cd3f
BeMain <benjamin.agardh@gmail.com>
2020-08-29 14:59:21 +0200

    Add __init__.py files to all packages

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-from game.gui import gui, main_menu
-
-import pyglet
-from pyglet import app
-
-import glooey
-
-
-window = pyglet.window.Window()
-gui = gui.GuiHandler(window)
-
-gui.open_menu()
-
-
-
-app.run()
+import game.terrain
